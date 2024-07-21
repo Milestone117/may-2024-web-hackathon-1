f112a990ec01b9de99647c01567cbab81656a077
milestone117 <milestone117@localhost.localdomain>
2024-07-21 19:30:35 +0300

    my_portifolio_website

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+let navbar = document.querySelector(".navbar");
 
-neonCursor({
-  el: document.getElementById('app'),
-  shaderPoints: 16,
-  curvePoints: 80,
-  curveLerp: 0.5,
-  radius1: 5,
-  radius2: 30,
-  velocityTreshold: 10,
-  sleepRadiusX: 100,
-  sleepRadiusY: 100,
-  sleepTimeCoefX: 0.0025,
-  sleepTimeCoefY: 0.0025
-})
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+
+document.addEventListener('mousemove', (event) => {
+  const neonCursor = document.querySelector('.neon-cursor');
+  neonCursor.style.left = event.clientX + 'px';
+  neonCursor.style.top = event.clientY + 'px';
+});
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-img", { origin: "bottom" });
+
+
